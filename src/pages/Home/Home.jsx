d7900548103d8fc4946de45ffe20c7336256f8eb
Simon Tessard <simon.tessard@gmail.com>
2023-03-11 16:38:42 +0100

    Added BarChart component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import data from '../../data/data.js'
 import Title from '../../components/Title'
 import AllStatsCards from '../../components/AllStatsCards'
+import BarChart from '../../components/BarChart'
 
 const ContentDiv = styled.div`
   display: flex;
@@ -23,7 +24,10 @@ function Home() {
         <SideNav />
         <DataDiv>
           <Title name={data.USER_MAIN_DATA[0].userInfos.firstName} />
-          <AllStatsCards data={data.USER_MAIN_DATA[0].keyData} />
+          <ContentDiv>
+            <BarChart data={data.USER_ACTIVITY[0]} />
+            <AllStatsCards data={data.USER_MAIN_DATA[0].keyData} />
+          </ContentDiv>
         </DataDiv>
       </ContentDiv>
     </div>
