0f860ae0e65ceac35eb1833a273a680c40c61ff0
Simon Tessard <simon.tessard@gmail.com>
2023-03-12 21:16:43 +0100

    Improved Barchart responsiveness and more

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -11,86 +11,51 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-  },
-]
-
 export default class Example extends PureComponent {
   constructor(props) {
     super(props)
 
     this.state = {
-      data: props.data,
+      data: props.data.sessions,
     }
   }
 
   render() {
     return (
-      <BarChart
-        width={700}
-        height={500}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="name" />
-        <YAxis orientation="right" />
-        <Tooltip />
-        <Legend verticalAlign="top" />
-        <Bar
+      <ResponsiveContainer width="70%" height={300}>
+        <BarChart
+          data={this.state.data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          barGap={8}
           barSize={7}
-          name="Poids (kg)"
-          dataKey="pv"
-          fill="#282D30"
-          radius={[10, 10, 0, 0]}
-        />
-        <Bar
-          barSize={7}
-          name="Calories brûlées (kCal)"
-          dataKey="uv"
-          fill="#E60000"
-          radius={[10, 10, 0, 0]}
-        />
-      </BarChart>
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis tickLine={false} tickMargin={15} dataKey="day" />
+          <YAxis
+            tickLine={false}
+            tickMargin={30}
+            dataKey="kilogram"
+            orientation="right"
+            tickCount={3}
+            type="number"
+            domain={['dataMin - 5', 'dataMax']}
+          />
+          <Tooltip />
+          <Legend iconType="circle" iconSize={8} height={80} align="right" verticalAlign="top" />
+          <Bar name="Poids (kg)" dataKey="kilogram" fill="#282D30" radius={[10, 10, 0, 0]} />
+          <Bar
+            name="Calories brûlées (kCal)"
+            dataKey="calories"
+            fill="#E60000"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     )
   }
 }
