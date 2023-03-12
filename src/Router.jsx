8165ec729b162332f2f9d52f4c97eb3f317f7d53
Simon Tessard <simon.tessard@gmail.com>
2023-03-13 00:49:19 +0100

    Added error page path

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Error from './pages/Error'
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/user/:id" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
