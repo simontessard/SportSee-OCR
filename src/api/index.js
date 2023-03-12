66257a04c7e2aef5d5e54b36c31a9c353037dbe8
Simon Tessard <simon.tessard@gmail.com>
2023-03-13 00:51:15 +0100

    Added try and catch to request

--- src/api/index.js
+++ src/api/index.js
@@ -3,28 +3,48 @@ const host = 'http://localhost:3000'
 
 // Fonction pour récupérer les informations d'un utilisateur
 export const fetchUserInfo = async (id) => {
-  const response = await fetch(host + '/user/' + id)
-  const data = await response.json()
-  return data.data
-}
+    try {
+      const response = await fetch(host + '/user/' + id)
+      const data = await response.json()
+      return data.data
+    } catch (error) {
+      console.error(error)
+      throw new Error('Une erreur est survenue lors de la récupération des informations utilisateur')
+    }
+  }
 
 // Fonction pour récupérer les sessions d'activité d'un utilisateur
 export const fetchUserActivity = async (id) => {
-  const response = await fetch(host + '/user/' + id + '/activity')
-  const data = await response.json()
-  return data.data
-}
+    try {
+      const response = await fetch(host + '/user/' + id + '/activity')
+      const data = await response.json()
+      return data.data
+    } catch (error) {
+      console.error(error)
+      throw new Error('Une erreur est survenue lors de la récupération des activités utilisateur')
+    }
+  }
 
 // Fonction pour récupérer la moyenne des sessions d'activité d'un utilisateur
 export const fetchUserAverage = async (id) => {
-  const response = await fetch(host + '/user/' + id + '/average-sessions')
-  const data = await response.json()
-  return data.data
-}
+    try {
+      const response = await fetch(host + '/user/' + id + '/average-sessions')
+      const data = await response.json()
+      return data.data
+    } catch (error) {
+      console.error(error)
+      throw new Error('Une erreur est survenue lors de la récupération de la moyenne des sessions activités utilisateur')
+    }
+  }
 
 // Fonction pour récupérer les performances d'un utilisateur
 export const fetchUsePerformance = async (id) => {
-  const response = await fetch(host + '/user/' + id + '/performance')
-  const data = await response.json()
-  return data.data
-}
+    try {
+      const response = await fetch(host + '/user/' + id + '/performance')
+      const data = await response.json()
+      return data.data
+    } catch (error) {
+      console.error(error)
+      throw new Error('Une erreur est survenue lors de la récupération de la moyenne des sessions activités utilisateur')
+    }
+  }
