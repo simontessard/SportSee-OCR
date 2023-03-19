ee7370543f088cadf589912989c876c747ad5a81
Simon Tessard <simon.tessard@gmail.com>
2023-03-19 20:44:51 +0100

    Removed useless import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from '../components/Header'
 import SideNav from '../components/SideNav'
 import styled from 'styled-components'
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const ContentDiv = styled.div`
   display: flex;
