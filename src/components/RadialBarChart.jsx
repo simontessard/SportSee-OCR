a16e34bd7025716b4618a4882974be61a7a52298
Simon Tessard <simon.tessard@gmail.com>
2023-03-19 21:45:03 +0100

    Transformed from Class to Function

--- src/components/RadialBarChart.jsx
+++ src/components/RadialBarChart.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts'
 
 import styled from 'styled-components'
@@ -8,58 +8,47 @@ const StyledResponsiveContainer = styled(ResponsiveContainer)`
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.0212249);
   border-radius: 5px;
 `
-const userScore = [{ id: '1', name: 'L1', value: 88, fill: 'red' }]
 
-export default class Example extends PureComponent {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      data: props.data,
-    }
-
-    console.log(this.state.data)
-  }
-
-  render() {
-    return (
-      <StyledResponsiveContainer width="33%" height={250}>
-        <RadialBarChart
-          startAngle={65}
-          cx="50%"
-          cy="50%"
-          innerRadius="70%"
-          outerRadius="90%"
-          barSize={15}
-          data={userScore}
+function RadialBarChartGoal(data) {
+  return (
+    <StyledResponsiveContainer width="33%" height={250}>
+      <RadialBarChart
+        startAngle={65}
+        cx="50%"
+        cy="50%"
+        innerRadius="70%"
+        outerRadius="90%"
+        barSize={15}
+        data={data.data}
+      >
+        <RadialBar minAngle={15} background clockWise dataKey="value" cornerRadius={15} />
+        <text
+          x={'50%'}
+          y={'45%'}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize={'26px'}
+          fontWeight={700}
         >
-          <RadialBar minAngle={15} background clockWise dataKey="value" cornerRadius={15} />
-          <text
-            x={'50%'}
-            y={'45%'}
-            textAnchor="middle"
-            dominantBaseline="middle"
-            fontSize={'26px'}
-            fontWeight={700}
-          >
-            {userScore[0].value}%
-          </text>
-          <text
-            x={'50%'}
-            y={'55%'}
-            textAnchor="middle"
-            dominantBaseline="middle"
-            fontSize={'16px'}
-            fontWeight={700}
-            fill={'#74798c'}
-          >
-            de votre objectif
-          </text>
-          <text x={60} y={40} textAnchor="middle" dominantBaseline="middle">
-            Score
-          </text>
-        </RadialBarChart>
-      </StyledResponsiveContainer>
-    )
-  }
+          88%
+        </text>
+        <text
+          x={'50%'}
+          y={'55%'}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize={'16px'}
+          fontWeight={700}
+          fill={'#74798c'}
+        >
+          de votre objectif
+        </text>
+        <text x={60} y={40} textAnchor="middle" dominantBaseline="middle">
+          Score
+        </text>
+      </RadialBarChart>
+    </StyledResponsiveContainer>
+  )
 }
+
+export default RadialBarChartGoal
