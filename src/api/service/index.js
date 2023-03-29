ed96b474730ebda2ba941b995bf966e31c3de8f6
Simon Tessard <simon.tessard@gmail.com>
2023-03-29 18:18:25 +0200

    Documenting functions

--- src/api/service/index.js
+++ src/api/service/index.js
@@ -2,28 +2,44 @@
 const host = 'http://localhost:3000'
 const urlAPI = host + '/user/'
 
-// Get personnal information about the user
+/**
+ * Function to get from the API the personal information about user specified
+ * @param {number} id - The id of the user
+ * @returns {object} - The data of the user
+ */
 export const fetchUserInfo = async (id) => {
     const response = await fetch(urlAPI + id)
     const data = await response.json()
     return data.data
   }
 
-// Get information of user's activities
+/**
+ * Function to get from the API the activies about user specified
+ * @param {number} id - The id of the user
+ * @returns {object} - The data of the user
+ */
 export const fetchUserActivity = async (id) => {
     const response = await fetch(urlAPI + id + '/activity')
     const data = await response.json()
     return data.data
   }
 
-// Get information of user's sessions activities
+/**
+ * Function to get from the API average sessions about user specified
+ * @param {number} id - The id of the user
+ * @returns {object} - The data of the user
+ */
 export const fetchUserSessions = async (id) => {
       const response = await fetch(urlAPI + id + '/average-sessions')
       const data = await response.json()
       return data.data
   }
 
-// Get information of user's performance
+/**
+ * Function to get from the API the performance about user specified 
+ * @param {number} id - The id of the user
+ * @returns {object} - The data of the user
+ */
 export const fetchUserPerformance = async (id) => {
       const response = await fetch(urlAPI + id + '/performance')
       const data = await response.json()
