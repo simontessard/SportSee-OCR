2bfc01002185690d6063f12f5f7f990ea8e8f89a
Simon Tessard <simon.tessard@gmail.com>
2023-03-29 13:58:46 +0200

    Added props type and default props

--- src/components/StatsCardList.jsx
+++ src/components/StatsCardList.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 import StatsCard from './StatsCard'
 import caloriesLogo from '../assets/card/fire.svg'
 import chickenLogo from '../assets/card/chicken.svg'
@@ -21,6 +22,21 @@ function StatsCardList(data) {
       <StatsCard count={data.lipidCount} type="lipides" image={cheeseburgerLogo} />
     </CardsContainer>
   )
+}
+
+StatsCardList.defaultProps = {
+  data: {
+    data: {
+      calorieCount: 1930,
+      proteinCount: 155,
+      carbohydrateCount: 290,
+      lipidCount: 50,
+    },
+  },
+}
+
+StatsCardList.propTypes = {
+  data: PropTypes.object,
 }
 
 export default StatsCardList
